a76af84b77ed668c5381e6235a1df4b2d7158ccb
Dmytro Nikolayev <dnikolayev@gmail.com>
2017-01-28 13:26:46 +0200

    CheckIO Labyrinth solution and finding the shortest way one

---
+++ Labyrinth/the-shortest-way.py
@@ -0,0 +1,205 @@
+#doing import of numpy, just for dimensions
+#forgot that 12x12 was preset :)
+import numpy
+​
+def checkio(maze_map):
+    
+    #function to check if the cell is ok to go through
+    def check_coord(coords):
+        if [0,0] <= coords <= [size_y,size_x] and not labyrinth[coords[0]][coords[1]]:
+                return True
+        return False
+        
+    #function for creating a hash for processed points with direction   
+    def processed_hash(coords, step):
+        return "".join(str(x) for x in [coords,step])
+    
+    #it was very difficult to get know, that coordinates in this task are (y,x) instead of common (x,y)
+    #because of this: steps are some fun
+    way_dict = {"W":(0,-1), "E": (0,1), "N":(-1,0), "S":(1,0)}
+​
+    #will store map here, needed to share data beteen functions
+    labyrinth    = []
+    #using lists instead of tuples to have ability to compare
+    start_point  = [1,1]
+    finish_point = [10,10]
+    size_x, size_y = (0,0)
+    # will store paths data here
+    check_point = []
+    #it will be a final solution
+    solution = ""
+    
+    # will put cells coords and direction of steps here as tasks to process
+    tasks = []
+    
+    # storage for processed data to not do it again
+    processed = set()
+    
+    #well, as I said earlier, I forgot that 12x12 is preset as size of the labyrinth
+    (size_x, size_y) = numpy.shape(maze_map)
+    
+    #-2 = -1 for 0 as start point and -1 as numpy counts from 1 :)
+    (size_x, size_y) = (size_x-2, size_y-2)
+    labyrinth = list(maze_map)
+    
+    # just doing prsetting of clear data for another array of arrays by shitty way
+    for x, x_arr in enumerate(labyrinth):
+        for y, y_val in enumerate(x_arr):
+            if y == 0:
+                check_point.append([])
+            check_point[x].append('')
+            
+    #applying start point as initial task, passing tuple into list
+    tasks.append((start_point, ''))
+    
+    #the main stuff, made as external function initally
+    #lets load our CPU while we have tasks.
+    while (len(tasks)):
+        #grabbing task
+        coords, direction = tasks.pop()
+        #special case for initial run                
+        if coords == [1, 1] and direction:
+            continue
+            
+        #checking if this point and direction is new
+        elif processed_hash(coords,direction) in processed:
+            continue 
+​
+        #checking if coords are good and cell is ok
+        if not check_coord(coords):
+            continue
+​
+        if direction:
+            #checking where did we come from and grabbing the path from that point
+            old_coords = [x - y for x, y in zip(coords, way_dict[direction])]
+    
+            #writing the way and checking that this is the shortest way to this point from start :)
+            if len(check_point[coords[0]][coords[1]]) == 0 or len(check_point[coords[0]][coords[1]]) > len(check_point[old_coords[0]][old_coords[1]])+1:
+                check_point[coords[0]][coords[1]] = check_point[old_coords[0]][old_coords[1]] + direction
+                #finish point found
+                if coords == finish_point: 
+                    #updated to select the shortest way:
+                    solution = check_point[coords[0]][coords[1]]
+                  
+                for step in way_dict:
+                    t = [x + y for x, y in zip(coords, way_dict[step])]
+​
+                    if check_coord(t):
+                        #asking to recount cells across to update for shorter solution
+                        if processed_hash(t,step) in processed:
+                            processed.remove(processed_hash(t,step))
+                        tasks.append((t, step))   
+            
+        #adding new point across current one    
+        for step in way_dict:
+            t = [x + y for x, y in zip(coords, way_dict[step])]
+            if not processed_hash(coords,step) in processed and check_coord(t):
+                    tasks.append((t, step))
+                    
+        #marking that we were here with direction            
+        processed.add(processed_hash(coords,direction))
+    return solution
+​
+​
+if __name__ == '__main__':
+    #This code using only for self-checking and not necessary for auto-testing
+    def check_route(func, labyrinth):
+        MOVE = {"S": (1, 0), "N": (-1, 0), "W": (0, -1), "E": (0, 1)}
+        #copy maze
+        route = func([row[:] for row in labyrinth])
+        pos = (1, 1)
+        goal = (10, 10)
+        for i, d in enumerate(route):
+            move = MOVE.get(d, None)
+            if not move:
+                print("Wrong symbol in route")
+                return False
+            pos = pos[0] + move[0], pos[1] + move[1]
+            if pos == goal:
+                return True
+            if labyrinth[pos[0]][pos[1]] == 1:
+                print("Player in the pit")
+                return False
+        print("Player did not reach exit")
+        return False
+​
+    # These assert are using only for self-testing as examples.
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+        [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "First maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Empty maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Up and down maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Dotted maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Need left maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "The big dead end."
+    print("The local tests are done.")
